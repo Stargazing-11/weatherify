7c096f5c705fbbb87ae6a64e7a1c1a08aa461946
Stargazing-11 <samuelkifle199314@gmail.com>
2023-06-06 22:28:47 +0300

    add utilities

---
+++ lib/core/utils/converter.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+const Color scaffoldBackground = Color(0xFFF2F2F2);
+const LinearGradient gradient = LinearGradient(
+  begin: Alignment.topCenter,
+  end: Alignment.bottomCenter,
+  colors: [Color(0xFF3C2DB9), Color(0xFF211772)],
+);
+
+const Color secondaryColor = Color.fromRGBO(255, 186, 37, 1);
+
+const Color blackColor = Colors.black;
+const Color primaryTextColor = Color(0xFF0D253C);
+const Color secondaryTextColor = Color(0xFF2d4379);
+const Color primaryColor = Color(0xFF376AED);
+const Color tertiaryColor = Color(0xFF5E5F6F);
+const Color darkPrimaryColor = Color(0xFF2151CD);
+const Color lightPrimaryColorGradient = Color(0xFF9CECFB);
+const Color darkPrimaryColorGradient = Color(0xFF376AED);
+const Color imageShadowColor = Color.fromARGB(255, 191, 191, 191);
+const Color BlueCardColor = Color(0xFF386BED);
+const Color homeBackgroundColor = Color(0xFFF8FAFF);
+const Color textColor2 = Color(0xFF424242);
+const Color textColor1 = Color(0xFF4D4A49);
+const Color transparentBlack = Colors.transparent;
+const Color textFieldTextColor = Color.fromARGB(255, 240, 239, 238);
+TextStyle titleTextStyle = const TextStyle(
+  color: Colors.black,
+  fontSize: 24.0,
+  fontFamily: 'Poppins',
+);
